60541e213877a40e34f105873f957da584a9750a
Benjamin Rabier <benjamin+git@rabier.dev>
2025-08-03 19:52:03 +0200

    tmp

--- modules/home/terminal/git.nix
+++ modules/home/terminal/git.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  system,
+  ...
+}: {
   home = {
     packages = with pkgs; [
       tig
@@ -8,14 +13,12 @@
   programs = {
     fish.shellAliases = {
       gs = "git status";
-      gca = "git commit --amend";
     };
 
-    gh = {
-      enable = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/4744
-      settings.version = 1;
-    };
+    mergiraf.enable = true;
+
+    gh.enable = true;
+
     git = {
       enable = true;
       difftastic = {
@@ -58,12 +61,74 @@
       settings = {
         user.name = "Benjamin Rabier";
         user.email = "benjamin+git@rabier.dev";
-        ui.diff-formatter = [
-          "difft"
-          "--color=always"
-          "$left"
-          "$right"
-        ];
+        template.git_push_bookmark = "\"brabier\"/ ++ change_id.short()";
+        ui = {
+          diff-formatter = [
+            "difft"
+            "--color=always"
+            "$left"
+            "$right"
+          ];
+          editor = "vi";
+          diff-editor = "diffview";
+        };
+        merge-tools = {
+          # See https://github.com/sindrets/diffview.nvim/issues/562#issuecomment-2867142680
+          diffview = {
+            program = "sh";
+            edit-args = [
+              "-c"
+              ''
+                set -eu
+                rm -f "$right/JJ-INSTRUCTIONS"
+                git -C "$left" init -q
+                git -C "$left" add -A
+                git -C "$left" commit -q -m baseline --allow-empty
+                mv "$left/.git" "$right"
+                (cd "$right"; vi -c DiffviewOpen)
+                git -C "$right" add -p
+                git -C "$right" diff-index --quiet --cached HEAD && { echo "No changes done, aborting split."; exit 1; }
+                git -C "$right" commit -q -m split
+                git -C "$right" restore . # undo changes in modified files
+                git -C "$right" reset .   # undo --intent-to-add
+                git -C "$right" clean -q -df # remove untracked files
+              ''
+            ];
+          };
+          mergiraf = {
+            program = "mergiraf";
+            merge-args = [
+              "merge"
+              "$base"
+              "$left"
+              "$right"
+              "-o"
+              "$output"
+              "--fast"
+            ];
+            merge-conflict-exit-codes = [1];
+            conflict-marker-style = "git";
+          };
+
+          aliases = {
+            d = ["diff"];
+            s = ["show"];
+            f = [
+              "git"
+              "fetch"
+            ];
+            # Find the closest ancestor with a bookmark pointing at it, and move it to the
+            # parent of the working copy.
+            tug = [
+              "bookmark"
+              "move"
+              "--from"
+              "heads(::@- & bookmarks())"
+              "--to"
+              "@-"
+            ];
+          };
+        };
       };
     };
   };
