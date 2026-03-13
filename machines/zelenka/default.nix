c02cc1377bac94f7299fa3b24bfe4d3d0630dc68
Benjamin Rabier <benjamin+git@rabier.dev>
2026-03-13 18:01:18 +0100

    nix flake update & vscode

--- machines/zelenka/default.nix
+++ machines/zelenka/default.nix
@@ -1,4 +1,5 @@
-{me, ...}: {
+{ me, ... }:
+{
   system.stateVersion = 6;
   home-manager.users.${me.userName} = {
     home.stateVersion = "26.05";
@@ -9,11 +10,26 @@
       "9960x-5090x2" = {
         forwardAgent = true;
       };
+      "vscode.9960x-4090x2" = {
+        forwardAgent = true;
+        hostname = "9960x-4090x2";
+        extraOptions = {
+          RemoteCommand = "/usr/bin/bash";
+        };
+      };
+      "vscode.9960x-5090x2" = {
+        forwardAgent = true;
+        hostname = "9960x-5090x2";
+        extraOptions = {
+          RemoteCommand = "/usr/bin/bash";
+        };
+      };
     };
   };
 
   homebrew = {
     casks = [
+      "visual-studio-code"
       "nvidia-nsight-systems"
       "chatwise"
     ];
