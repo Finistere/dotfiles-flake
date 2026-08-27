9c2ca91067bce3ba0a951585b6c4e371db6f270f
Benjamin Rabier <benjamin@rabier.dev>
2026-08-27 13:32:48 +0200

    zelenka: add gb300

--- machines/zelenka/default.nix
+++ machines/zelenka/default.nix
@@ -1,22 +1,38 @@
 { me, ... }:
 let
-  settings = builtins.listToAttrs (
-    map
-      (name: {
-        inherit name;
-        value = {
-          ForwardAgent = true;
-        };
-      })
-      [
-        "9960x-4090x2"
-        "9960x-5090x2"
-        "9960x-b70x2"
-        "9985wx-5090x4"
-        "9950x-tt"
-        "9950x-radeon-pro"
-      ]
-  );
+  settings =
+    builtins.listToAttrs (
+      map
+        (name: {
+          inherit name;
+          value = {
+            ForwardAgent = true;
+          };
+        })
+        [
+          "9960x-4090x2"
+          "9960x-5090x2"
+          "9960x-b70x2"
+          "9985wx-5090x4"
+          "mi300"
+          "9950x-tt"
+          "9950x-radeon-pro"
+        ]
+    )
+    // builtins.listToAttrs (
+      map
+        (name: {
+          inherit name;
+          value = {
+            ForwardAgent = true;
+            User = "benjamin";
+          };
+        })
+        [
+          "gb300-1"
+          "gb300-2"
+        ]
+    );
 in
 {
   system.stateVersion = 6;
